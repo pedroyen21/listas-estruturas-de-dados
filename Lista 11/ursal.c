5005773bd12f8dcde43536d5b29ef4396fc53ca4
pedrosbhy <pedrosbhy93@gmail.com>
2023-01-21 17:15:39 -0300

    lista 11 - ursal completa

--- Lista 11/ursal.c
+++ Lista 11/ursal.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define exch(A, B) {Item t = A; A = B; B = t;}
-#define ncandidatos 90000
+#define ncandidatos 100000
 
 typedef struct {
     long int codigo;
@@ -11,7 +11,6 @@ typedef struct {
 } candidato;
 
 typedef candidato Item;
-
 
 int median(Item *v, int l, int m, int r) {
     int vm = v[m].votos;
@@ -55,30 +54,46 @@ void quicksort(Item *v, int l, int r) {
 
 void quickselect(Item *v, int p, int l, int r) {
     int j = partition(v, l, r);
-
+    //printf("partition l: %d r: %d j: %d ", l, r, j);
     if(j == p) return;
-    if(j < p ) 
+    if(j < p ) {
         quickselect(v, p, j + 1, r);
-    else    
+    }   
+    else {    
         quickselect(v, p, l, j - 1);
+    }
 }
 
 
 void printvotos(candidato *v) {
     for(int i = 0; i < ncandidatos; i++) {
         if(v[i].votos > 0) 
-            printf("[%ld,%d]\n", v[i].codigo, v[i].votos);
+            printf("pos %i [%ld,%d]\n",i, v[i].codigo, v[i].votos);
     }
 }
+void resultado_51(candidato *v, int l, int r) {
+    int vts = 0;
+    candidato max = v[l];
+    for(int i = l; i <= r; i++) {
+        if(v[i].votos > 0) vts += v[i].votos;
 
-void resultado(candidato *v, int p, int l, int r){
-    quickselect(v, p, l, r);
-    printf("quickselect");
-    quicksort(v, l, p);
-    printf("quicksort");
-    for(int i = l; i <= p; i++) {
-        printf("%ld", v[i].codigo);
+        if(greater(v[i], max))
+            max = v[i];
     }
+    float res = (float)max.votos / (float)vts;
+    //printf("max.votos : %d, votos %d\n", max.votos, vts);
+    //printf("res: %f\n", res);
+    if(res >= 0.51) printf("%ld\n", max.codigo);
+    else printf("Segundo turno\n");
+}
+void resultado_maioria_simples(candidato *v, int p, int l, int r){
+    p += l - 1;
+    quickselect(v, p, l, r);
+    quicksort(v, l, p);
+    for(int i = l; i <= p; i++) {
+        printf("%ld ", v[i].codigo);
+    }
+    printf("\n");
 };
 
 int main(void) {
@@ -107,11 +122,13 @@ int main(void) {
             }
         }
     }
+    printf("%d ", votos_validos);
+    printf("%d\n", votos_invalidos);
 
-    printvotos(candidatos);
-    resultado(candidatos, 0, 10, 99);
-    printvotos(candidatos);
+    //printvotos(candidatos);
+    resultado_51(candidatos, 10, 99);
+    resultado_maioria_simples(candidatos, sv, 100, 999);
+    resultado_maioria_simples(candidatos, dfv, 1000, 9999);
+    resultado_maioria_simples(candidatos, dev, 10000, 99999);
     
-    printf("validos: %d\n", votos_validos);
-    printf("invalidos: %d\n", votos_invalidos);
 }
